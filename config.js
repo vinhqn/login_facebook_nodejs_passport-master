3a8ee386c3561492afd23ea3c1e6de420447695b
Vinh Nguyen <>
2021-03-11 14:16:24 +0700

    add code

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "160428422575665",
+  "facebook_api_secret"   :     "d17790a216d6d88125a759e47a50ddc5",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     true,
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "login_passport"
+}
